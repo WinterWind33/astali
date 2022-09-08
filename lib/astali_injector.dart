45ca1e164271c8cc7c5e66bb12889a4290607f01
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-08 20:05:54 +0200

    Added astali_injector.dart file.

---
+++ lib/astali_injector.dart
@@ -0,0 +1,5 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+
+/// Represents an injector class that provides objects instances for the correct
+/// execution of the main astali app.
+abstract class AstaliInjector {}
