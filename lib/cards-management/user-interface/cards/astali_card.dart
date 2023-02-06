bae4ba17927d06464d7247161ef345396124c613
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-06 21:42:49 +0100

    Added AstaliCard and _AstaliCardState classes.

--- lib/cards-management/user-interface/cards/astali_card.dart
+++ lib/cards-management/user-interface/cards/astali_card.dart
@@ -1,1 +1,16 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+import 'package:flutter/material.dart';
+
+class AstaliCard extends StatefulWidget {
+  const AstaliCard({super.key});
+
+  @override
+  State<AstaliCard> createState() => _AstaliCardState();
+}
+
+class _AstaliCardState extends State<AstaliCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Card();
+  }
+}
