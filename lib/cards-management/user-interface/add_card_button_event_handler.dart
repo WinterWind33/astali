5edba4db000ae9d93bed461a7f91dc8c4150b58e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-06 21:10:02 +0100

    Added AddCardButtonEventHandler abstract class.

--- lib/cards-management/user-interface/add_card_button_event_handler.dart
+++ lib/cards-management/user-interface/add_card_button_event_handler.dart
@@ -1,1 +1,10 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+import 'package:flutter/material.dart';
+
+/// Represents the callbacks that are triggered when the Add Card button is
+/// tapped.
+abstract class AddCardButtonEventHandler {
+  /// Called when the Add Card buton is clicked by the user.
+  void onAddCardClicked(BuildContext buildContext);
+}
