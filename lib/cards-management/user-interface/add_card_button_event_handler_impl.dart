630ad6fbe8c325551c3df0c5a604208d9156346e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-06 21:14:08 +0100

    Added AddCardButtonEventHandlerImpl class.

--- lib/cards-management/user-interface/add_card_button_event_handler_impl.dart
+++ lib/cards-management/user-interface/add_card_button_event_handler_impl.dart
@@ -1,1 +1,12 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+import 'package:astali/cards-management/user-interface/add_card_button_event_handler.dart';
+
+import 'package:flutter/material.dart';
+
+class AddCardButtonEventHandlerImpl implements AddCardButtonEventHandler {
+  const AddCardButtonEventHandlerImpl();
+
+  @override
+  void onAddCardClicked(BuildContext buildContext) {}
+}
