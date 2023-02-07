d971c0f59922d627c6d8976705e1385c1e003fff
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-07 22:08:26 +0100

    Added FSMState interface.

--- lib/fsm/fsm_state.dart
+++ lib/fsm/fsm_state.dart
@@ -1,1 +1,17 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+/// Represents the interface of a finite state machine
+/// state. The state is represented by a name that
+/// has SymbolType as data type.
+abstract class FSMState<SymbolType> {
+
+  /// Retrieves the name of this state
+  SymbolType getStateName();
+
+  /// Called when performing a transition to this state.
+  void onStateEnter();
+
+  /// Called when performing a transition from thi state
+  /// to another.
+  void onStateLeave();
+}
