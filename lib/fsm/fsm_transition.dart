077f7e0d7e7dbb905a9c0204d78dad885a33a1df
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-07 22:28:37 +0100

    Added FSMTransition class.

--- lib/fsm/fsm_transition.dart
+++ lib/fsm/fsm_transition.dart
@@ -1,1 +1,27 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+import 'fsm_state.dart';
+
+abstract class FSMStateResolver<SymbolType> {
+  FSMState<SymbolType>? getState(final SymbolType stateName);
+}
+
+
+class FSMTransition<SymbolType> {
+  FSMTransition(FSMState<SymbolType> startingState, final FSMStateResolver<SymbolType> stateResolver) :
+    _startingState = startingState,
+    _stateResolver = stateResolver;
+
+  FSMState<SymbolType> execute(final SymbolType finalStateName) {
+    FSMState<SymbolType>? finalState = _stateResolver.getState(finalStateName);
+    assert(finalState != null);
+
+    _startingState.onStateLeave();
+    finalState!.onStateEnter();
+
+    return finalState;
+  }
+
+  FSMState<SymbolType> _startingState;
+  final FSMStateResolver<SymbolType> _stateResolver;
+}
