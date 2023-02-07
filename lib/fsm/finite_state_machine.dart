43d4abfb4e6b54b8b9799ca724001c406c85fc1a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-07 22:04:25 +0100

    Added initial FiniteStateMachine class definition.

--- lib/fsm/finite_state_machine.dart
+++ lib/fsm/finite_state_machine.dart
@@ -1,1 +1,16 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+class FiniteStateMachine<SymbolType> {
+  FiniteStateMachine(Set<SymbolType> alphabet)
+    : _alphabet = alphabet;
+
+  // === Getters ===
+
+  Set<SymbolType> getAlphabet() {
+    return _alphabet;
+  }
+
+  // ==== PRIVATE ====
+
+  final Set<SymbolType> _alphabet;
+}
