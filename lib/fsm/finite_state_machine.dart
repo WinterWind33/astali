57babe77bb53926d81a4d387463a4b1c5825e851
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-07 22:15:33 +0100

    Added initial status to finite state machine.

--- lib/fsm/finite_state_machine.dart
+++ lib/fsm/finite_state_machine.dart
@@ -1,8 +1,11 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
 
+import 'fsm_state.dart';
+
 class FiniteStateMachine<SymbolType> {
-  FiniteStateMachine(Set<SymbolType> alphabet)
-    : _alphabet = alphabet;
+  FiniteStateMachine(Set<SymbolType> alphabet, FSMState initialState):
+    _alphabet = alphabet,
+    _currentState = initialState;
 
   // === Getters ===
 
@@ -10,7 +13,18 @@ class FiniteStateMachine<SymbolType> {
     return _alphabet;
   }
 
+  SymbolType getCurrentStateName() {
+    return _currentState.getStateName();
+  }
+
   // ==== PRIVATE ====
 
   final Set<SymbolType> _alphabet;
+  FSMState _currentState;
+}
+
+/// Checks wheter or not the given finite state machine is in the state
+/// represented by StateName.
+bool isInState<SymbolType>(final FiniteStateMachine<SymbolType> fsm, final SymbolType stateName) {
+  return fsm.getCurrentStateName() == stateName;
 }
