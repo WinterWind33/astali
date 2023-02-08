8a597a1ce19d55cd71c3f574dd9ee9d4351ca914
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-08 21:40:11 +0100

    Added BulletinBoardScene stateful widget.

--- lib/scenes/bulletin_board_scene.dart
+++ lib/scenes/bulletin_board_scene.dart
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 typedef OnCardAddEvent = VoidCallback;
@@ -30,4 +31,26 @@ class BulletinBoardScenePresentation extends StatelessWidget {
       body: _createBody(),
     );
   }
+}
+
+class BulletinBoardScene extends StatefulWidget {
+  const BulletinBoardScene({super.key});
+
+  @override
+  State<BulletinBoardScene> createState() => _BulletinBoardState();
+}
+
+class _BulletinBoardState extends State<BulletinBoardScene> {
+  @override
+  Widget build(Object context) {
+    return MouseRegion(
+      onHover: _onMouseHover,
+      child: BulletinBoardScenePresentation(onCardAddEvent: _onCardAddEvent),
+    );
+  }
+
+  void _onCardAddEvent() {}
+
+  void _onMouseHover(PointerHoverEvent pointerHoverEvent) {}
+
 }
