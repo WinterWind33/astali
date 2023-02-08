0cf9da1570af8a624d74a7c62354ebfa5becfb11
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-08 21:14:59 +0100

    Added BulletinBoardScene stateless widget.

--- lib/scenes/bulletin_board_scene.dart
+++ lib/scenes/bulletin_board_scene.dart
@@ -1,1 +1,26 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+import 'package:flutter/material.dart';
+
+typedef OnCardAddEvent = VoidCallback;
+
+class BulletinBoardScene extends StatelessWidget {
+  const BulletinBoardScene({required this.onCardAddEvent, super.key});
+
+  final OnCardAddEvent onCardAddEvent;
+
+  FloatingActionButton _createAddCardButton() {
+    return FloatingActionButton(
+      onPressed: onCardAddEvent,
+      tooltip: "Add a card",
+      child: const Icon(Icons.add),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: _createAddCardButton(),
+    );
+  }
+}
