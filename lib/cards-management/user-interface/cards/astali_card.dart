9134636eca5af2b88192185dd14e615edfacee40
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-11 17:35:32 +0100

    Improved card visualization.

--- lib/cards-management/user-interface/cards/astali_card.dart
+++ lib/cards-management/user-interface/cards/astali_card.dart
@@ -1,15 +1,22 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+// Core and engine
+import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter/material.dart';
+typedef OnMarkdownTextChanged = void Function(String);
 
 class AstaliCardPresentation extends StatelessWidget {
   const AstaliCardPresentation({
     required this.cardPosition,
+    required this.onMarkdownTextChanged,
+    required this.markdownText,
     super.key
   });
 
   final Point<double> cardPosition;
+  final OnMarkdownTextChanged onMarkdownTextChanged;
+  final String markdownText;
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +30,40 @@ class AstaliCardPresentation extends StatelessWidget {
             height: 200,
             child: Card(
               shadowColor: Colors.black,
-              color: Colors.blue,
+              color: Colors.yellow[200],
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: const [
-                  Text("Card title"),
-                  Text("Card description")
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Title"
+                      ),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textInputAction: TextInputAction.next,
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Description",
+                        border: InputBorder.none,
+
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                      textInputAction: TextInputAction.newline,
+                      minLines: 1,
+                      maxLines: 5
+                    )
+                  )
                 ]
               ),
             )
@@ -51,8 +87,19 @@ class AstaliCard extends StatefulWidget {
 }
 
 class _AstaliCardState extends State<AstaliCard> {
+  String _cardDescription = "";
+
   @override
   Widget build(BuildContext context) {
-    return AstaliCardPresentation(cardPosition: widget.cardPosition);
+    return AstaliCardPresentation(
+      cardPosition: widget.cardPosition,
+      onMarkdownTextChanged: _onDescriptionChanged,
+      markdownText: _cardDescription);
+  }
+
+  void _onDescriptionChanged(String value) {
+    setState(() {
+      _cardDescription = value;
+    });
   }
 }
