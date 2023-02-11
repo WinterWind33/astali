0f5e0713d5ad5e5bdc24b231703d151f1ddf31aa
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-11 18:01:50 +0100

    Added pointer_events.dart file.

---
+++ lib/input-management/pointer_events.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+
+import 'package:flutter/gestures.dart';
+
+/// Sets of event for pointers.
+
+typedef OnPointerHoverEvent = void Function(PointerHoverEvent);
+typedef OnPointerUpEvent = void Function(PointerUpEvent);
+typedef OnPointerDownEvent = void Function(PointerDownEvent);
