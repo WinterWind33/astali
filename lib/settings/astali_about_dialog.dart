323a556572e40ab4a5a8b563476152ec5f722e15
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-11 17:52:30 +0100

    Updated documentation.

--- lib/settings/astali_about_dialog.dart
+++ lib/settings/astali_about_dialog.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 import 'package:flutter/material.dart';
 
+/// Shows the about dialog with application information and
+/// legal notices.
 void showAstaliAboutDialog(BuildContext context) {
   showAboutDialog(
         context: context,
