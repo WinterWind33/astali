df75960f188afafbf2d105ab2e0ac605a4e93441
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-13 21:20:20 +0100

    Updated documentation.

--- lib/fsm/fsm_transition.dart
+++ lib/fsm/fsm_transition.dart
@@ -2,16 +2,24 @@
 
 import 'fsm_state.dart';
 
+/// Represents the basic interface of a state resolver, i.e. an object
+/// that, given a symbol name, it retrieves the corresponding FSM state.
 abstract class FSMStateResolver<SymbolType> {
   FSMState<SymbolType> getState(final SymbolType stateName);
+
+  /// Retrieves all the names for the states that are supported
+  /// by this resolver.
   Set<SymbolType> getAlphabet();
 }
 
-
+/// Represents a transition between two states of a finite state machine
 abstract class FSMTransition<SymbolType> {
   FSMState<SymbolType> execute(FSMState<SymbolType> startingState, final SymbolType finalStateName);
 }
 
+/// Represents the basic transition between two states:
+///  - onStateLeave() called on the starting state;
+///  - onStateEnter() called on the final state;
 class FSMSimpleTransition<SymbolType> implements FSMTransition<SymbolType> {
   FSMSimpleTransition(final FSMStateResolver<SymbolType> stateResolver) :
     _stateResolver = stateResolver;
@@ -29,6 +37,8 @@ class FSMSimpleTransition<SymbolType> implements FSMTransition<SymbolType> {
   final FSMStateResolver<SymbolType> _stateResolver;
 }
 
+/// Represents a particular transition between and invalid starting state (typically a
+/// deferred initialization satte) and the first valid state of a FSM.
 class FSMTransitionToInitialState<SymbolType> implements FSMTransition<SymbolType> {
   const FSMTransitionToInitialState(final FSMStateResolver<SymbolType> stateResolver) :
     _stateResolver = stateResolver;
