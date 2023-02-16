bdd6587e150138bf5509267d8362b8eecb77cd15
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-16 18:51:12 +0100

    Added new definition of BulletinBoardCardID and a generator class.

--- lib/cards-management/bulletin-board-cards/bulletin_board_card_id.dart
+++ lib/cards-management/bulletin-board-cards/bulletin_board_card_id.dart
@@ -1,1 +1,29 @@
 // Copyright (C) 2023 Andrea Ballestrazzi
+
+/// The ID of a bulletin board card.
+typedef BulletinBoardCardID = int;
+
+/// Represents the basic interface of an ID generator
+/// for a bulletin board card.
+abstract class BulletinBoardCardIDGenerator {
+  /// Generates a card ID.
+  BulletinBoardCardID generateID();
+}
+
+/// Simple Card ID generator that generates a new and unique ID for each call.
+/// The uniqueness is object-wide, so multiple generators may generate the same ID
+/// but one generator can't generate two equal IDs.
+class BulletinBoardCardIDSimpleGenerator
+    implements BulletinBoardCardIDGenerator {
+  BulletinBoardCardIDSimpleGenerator() : _currentCardID = 0;
+
+  @override
+  BulletinBoardCardID generateID() {
+    // We reserve the zero for future uses. We start from 1.
+    _currentCardID++;
+
+    return _currentCardID;
+  }
+
+  BulletinBoardCardID _currentCardID;
+}
