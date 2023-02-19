e6217f1f53ce47dd643fbb72779c5f74e09aa833
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-19 10:37:48 +0100

    Added initial implementation of BulletinBoardCardsManager and its implementation.

--- lib/cards-management/bulletin-board-cards/bulletin_board_cards_manager.dart
+++ lib/cards-management/bulletin-board-cards/bulletin_board_cards_manager.dart
@@ -1,1 +1,52 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+
+import 'presentation/bulletin_board_card.dart';
+import 'bulletin_board_card_id.dart';
+
+typedef BulletinBoardCards = Map<BulletinBoardCardID, BulletinBoardCard>;
+
+abstract class BulletinBoardCardsManager {
+  BulletinBoardCards getBulletinBoardCards();
+
+  void addCard(BulletinBoardCard newCard);
+
+  void updateCard(BulletinBoardCard card);
+
+  void deleteCard(BulletinBoardCardID cardID);
+}
+
+class BulletinBoardCardsManagerImpl implements BulletinBoardCardsManager {
+  BulletinBoardCardsManagerImpl() : _bulletinBoardCards = {};
+
+  @override
+  void addCard(BulletinBoardCard newCard) {
+    assert(!_containsCard(newCard.cardID));
+
+    _bulletinBoardCards.putIfAbsent(newCard.cardID, () => newCard);
+  }
+
+  @override
+  void deleteCard(BulletinBoardCardID cardID) {
+    assert(_containsCard(cardID));
+
+    _bulletinBoardCards.remove(cardID);
+  }
+
+  @override
+  BulletinBoardCards getBulletinBoardCards() {
+    return _bulletinBoardCards;
+  }
+
+  @override
+  void updateCard(BulletinBoardCard card) {
+    assert(_containsCard(card.cardID));
+
+    _bulletinBoardCards.update(card.cardID, (value) => card);
+  }
+
+  bool _containsCard(final BulletinBoardCardID cardID) {
+    return _bulletinBoardCards.containsKey(cardID);
+  }
+
+  final BulletinBoardCards _bulletinBoardCards;
+}
