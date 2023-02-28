7d2376555f2b365bd40cda74ba05438f5a5a2194
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-02-28 18:02:13 +0100

    Added BulletinBoardCardRawInputController.

--- lib/cards-management/bulletin-board-cards/bulletin_board_card_input.dart
+++ lib/cards-management/bulletin-board-cards/bulletin_board_card_input.dart
@@ -1,1 +1,19 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+
+import 'bulletin_board_card_id.dart';
+
+// Core and engine
+// Input managment
+import 'package:flutter/gestures.dart';
+
+/// Represents the basic interface of a card controller that can handle raw
+/// inputs for a bulletin board card.
+abstract class BulletinBoardCardRawInputController {
+  void onPointerUpOnCard(
+      BulletinBoardCardID cardID, PointerUpEvent pointerUpEvent);
+
+  void onPointerDownOnCard(
+      BulletinBoardCardID cardID, PointerDownEvent pointerDownEvent);
+
+  void onCardFocusChanged(BulletinBoardCardID cardID, bool bHasFocus);
+}
