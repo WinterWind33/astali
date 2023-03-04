22145b47e97069637024f5d8ea70670ff2e9544b
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-03-04 08:47:36 +0100

    Removed unused mocks files.

--- test/test-doubles/fsm/fsm_state.mocks.dart
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023 Andrea Ballestrazzi
-
-import 'package:astali/fsm/fsm_state.dart';
-import 'package:mockito/mockito.dart';
-
-class FSMStateMock<SymbolType> extends Mock implements FSMState<SymbolType> {}
