d8e636849afb61b1fa77b43048d88a3b1bd3fb15
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-03-04 08:25:58 +0100

    Added FSMStateMock class.

--- test/test-doubles/fsm/fsm_state.mocks.dart
+++ test/test-doubles/fsm/fsm_state.mocks.dart
@@ -1,1 +1,6 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+
+import 'package:astali/fsm/fsm_state.dart';
+import 'package:mockito/mockito.dart';
+
+class FSMStateMock<SymbolType> extends Mock implements FSMState<SymbolType> {}
